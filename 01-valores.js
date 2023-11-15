dafdbcfc95eb57b8149f01808cde64d953027552
mmmelomano <kristian.rojasquiroz@gmail.com>
2023-11-15 14:20:13 -0500

    ceroacien.js

---
+++ 01-valores.js
@@ -0,0 +1,17 @@
+//Valores
+
+//String / Cadena de texto
+"Carlos Valero"
+'Carlos Valero'
+
+//Valores numericos
+12
+12.1
+
+//Valores booleanos
+true
+false
+
+//Valores vacios
+null
+undefined
---
+++ 02-variables.js
@@ -0,0 +1,31 @@
+//Variables
+
+// palabras reservadas var let const
+// el signo = es asignacion
+var nombre = 'carlos valero';
+var edad = 23;
+
+//declarar una variable
+var edad;
+//inicializada
+edad = 23;
+
+//Varibles complejas
+
+//variables tipo objeto: se pueden almacenar distintos tipos de propiedades
+var persona = {
+    nombre: 'carlos',
+    apellido: 'valera',
+    edad: 23
+};
+
+//Array - Arreglo : puede almacenar varios valores
+var amigos = ['Victor', 'Paula', 'Juan', 'Raquel'];
+var edades = [24, 21, 25, 19];
+
+var amigos = [
+    { nombre: 'Victor', edad: 24},
+    { nombre: 'Paula' , edad: 21},
+    { nombre: 'Juan', edad: 25},
+    { nombre: 'Raquel', edad: 19}
+];
